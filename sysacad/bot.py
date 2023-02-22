e1cdbc77e96ebd5d926552e85cb2a8fd36baf9c6
Julian A. Albanesi <andresalbanesi@gmail.com>
2023-02-21 23:25:13 -0300

    Apunto de cambiar mucho la sintaxis

--- sysacad/bot.py
+++ sysacad/bot.py
@@ -2,20 +2,28 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
+legajo = 51026
+
 browser = webdriver.Firefox()
 browser.get("https://www.alumnos.frro.utn.edu.ar/")
+print("entro a la web")
 
-time.sleep(3)
+time.sleep(2)
 
 user = browser.find_elements('xpath','//*[@id="page-container"]/div[2]/form/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[1]/td[2]/input')
-user[0].send.keys('51026')
-
+print("encontro el elemento legajo")
+time.sleep(0.5)
+user.clear()
+print("limpio el input de legajo")
+time.sleep(0.5)
+user.send_keys(legajo + Keys.ENTER)
+print("Escribio el legajo")
 time.sleep(0.5)
 
 user = browser.find_elements ('xpath','//*[@id="page-container"]/div[2]/form/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[2]/td[2]/input')
 with open('password.txt', 'r') as myfile:
     password = myfile.read().replace('\n', '')
-user.send_keys(password)
+user.send_keys(password + Keys.ENTER)
 
 time.sleep(0.5)
 
