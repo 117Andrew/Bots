50ee1ae0956c94f96d9f3066920604f64c3bb67b
Julian A. Albanesi <andresalbanesi@gmail.com>
2023-02-22 22:49:09 -0300

    While agregado al bot, aun funciona, falta automatizar "F5"

--- sysacad/bot.py
+++ sysacad/bot.py
@@ -3,20 +3,26 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
-browser = webdriver.Firefox()
-browser.get("https://www.alumnos.frro.utn.edu.ar/")
-print("entro a la web")
+browser = webdriver.Firefox()                                                                      
+#Drivers para firefox
+browser.get("https://www.alumnos.frro.utn.edu.ar/")                                                
+#Abre el navegador en el url espeficicado
 
 time.sleep(2)
+#Da un retardo de 2 segundos
+url = browser.current_url
 
-with open('legajo.txt', 'r') as myfile:
-    legajo = myfile.read().replace('\n', '')
-browser.find_element(By.NAME, "legajo").send_keys(legajo)
-print("Escribio el legajo")
-time.sleep(0.5)
+while (url == browser.current.url):
+    with open('legajo.txt', 'r') as myfile:
+        legajo = myfile.read().replace('\n', '')
+    #Abre el archivo que contiene el legajo y lo asigna a una variable
+    browser.find_element(By.NAME, "legajo").send_keys(legajo)
+    #Encuentra la casilla donde se escribe el legajo y lo ingresa
+    time.sleep(0.5)
 
-with open('password.txt', 'r') as myfile:
-    password = myfile.read().replace('\n', '')
-browser.find_element(By.NAME, "password").send_keys(password + Keys.ENTER)
-
-time.sleep(5)
+    with open('password.txt', 'r') as myfile:
+        password = myfile.read().replace('\n', '')
+    #Abre el archivo que contiene la contraseña y la asigna a una variable
+    browser.find_element(By.NAME, "password").send_keys(password + Keys.ENTER)
+    #Encuentra la casilla donde se escribe la contraseña y la ingresa
+    time.sleep(5)
